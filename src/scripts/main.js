abc167e7b86e7a33abe7b11b4851729184a9f757
Gabriela Zimmermann <zmngabriela@gmail.com>
2024-06-03 23:57:32 +0200

    adding countdown

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('oi');
+AOS.init();
+
+const eventDate = new Date("Dec 12, 2024 19:00:00");
+const eventTimeStamp = eventDate.getTime();
+
+const timeCount = setInterval(function() {
+    const now = new Date();
+    const currentTimeStamp = now.getTime();
+
+    const timeUntilEvent = eventTimeStamp - currentTimeStamp;
+
+    const daysUntilEvent = Math.floor(timeUntilEvent / (1000 * 60 * 60 * 24));
+    const hoursUntilEvent = Math.floor((timeUntilEvent % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutesUntilEvent = Math.floor(timeUntilEvent % (1000 * 60 * 60) / (1000 * 60));
+    const secondsUntilEvent = Math.floor(timeUntilEvent % (1000 * 60) / 1000);
+
+    document.querySelector('.counting').innerHTML = `${daysUntilEvent}d ${hoursUntilEvent}h ${minutesUntilEvent}m ${secondsUntilEvent}s`;
+}, 1000);
